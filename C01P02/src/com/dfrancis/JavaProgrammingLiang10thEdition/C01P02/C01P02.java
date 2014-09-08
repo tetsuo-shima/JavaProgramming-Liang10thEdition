1e4d1b52f5ecb7bff6b92311937aaf685a486993
Dwight Francis <dfrancis0@gmail.com>
2014-09-08 17:43:48 -0400

    Initial for problem 2

---
+++ C01P02/src/com/dfrancis/JavaProgrammingLiang10thEdition/C01P02/C01P02.java
@@ -0,0 +1,13 @@
+package com.dfrancis.JavaProgrammingLiang10thEdition.C01P02;
+
+public class C01P02 {
+
+    public static void main(String[] args) {
+	// write your code here
+        String welcome = "Welcome to Java";
+
+        for (int index = 0; index < 5; index++) {
+            System.out.println(welcome);
+        }
+    }
+}
