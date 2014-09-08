578c83e3b29f1c53e6ce897dec112087a5d9708a
Dwight Francis <dfrancis0@gmail.com>
2014-09-08 17:08:28 -0400

    Initial for problem 1

---
+++ C01P01/src/org/dfrancis/JavaProgrammingLiang10thEdition/C01P01/C01P01.java
@@ -0,0 +1,13 @@
+package org.dfrancis.JavaProgrammingLiang10thEdition.C01P01;
+
+public class C01P01 {
+
+    public static void main(String[] args) {
+	// write your code here
+        String java = "Welcome to Java";
+        String compsci = "Welcome to Computer Science";
+        String prog = "Programming is fun";
+
+        System.out.printf("%s\n%s\n%s\n", java, compsci, prog);
+    }
+}
